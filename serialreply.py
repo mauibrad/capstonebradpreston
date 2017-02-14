6b170d3cb2af70e8ba9259afaf3b965a1f3bde4b
mauibrad <bradleypittman94@gmail.com>
2017-02-14 12:12:06 -1000

    Update serialreply.py

--- serialreply.py
+++ serialreply.py
@@ -2,10 +2,10 @@ import tweepy #python jewel for twitter API
 from time import sleep #used to set tweet interval
 import serial
 
-consumer_key = 'ZQnIqEMo98yRSfYlLHAaPposO'
-consumer_secret ='QtOPS01OJCAaUIVeRL8P3vwd0rv5AoXkTMfLq1ZgrkIZ2UVviJ'
-access_token = '822328817565868032-VPuIk19SMb8zfNTozn9vw0zkOv0GEHi'
-access_token_secret = 'dCqeTWQC5MR7nek47kK3y7bL8CMPq2ARsKpKge1TT1gY1'
+consumer_key = 'ZQnIqEMoaPposO'
+consumer_secret ='QtOPS0TMfLq1ZgrkIZ2UVviJ'
+access_token = '8223288Ov0GEHi'
+access_token_secret = 'dCqe1gY1'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
