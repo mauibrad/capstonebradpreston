540cfd90384756bbfcc3938b026ab3eb91b96896
PrestonYR <pyr@hawaii.edu>
2017-02-14 12:09:48 -1000

    Add files via upload

---
+++ Preston_GTR.ino
@@ -0,0 +1,122 @@
+      // Feather9x_TX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (transmitter)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example Feather9x_RX
+
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+ #include <SoftwareSerial.h>
+//Data wire
+#define ONE_WIRE_BUS 10
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+//GPS
+SoftwareSerial mySerial(11, 12);
+#define PMTK_SET_NMEA_UPDATE_1HZ  "$PMTK220,1000*1F"
+#define PMTK_SET_NMEA_OUTPUT_RMCONLY "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29"
+
+/* for feather32u4 */
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 7
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+void setup() 
+{
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+   mySerial.begin(57600);
+  
+  while (!Serial);
+  Serial.begin(9600);
+  delay(100);
+
+  Serial.println("Feather LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+    //GPS
+    mySerial.begin(9600); 
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  rf95.setTxPower(23, false);
+}
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+void loop()
+{
+  // Send a message
+  sensors.requestTemperatures();
+  char rad = sensors.getTempCByIndex(0);
+  char radiopacket[30];
+  dtostrf(rad, 6, 2, radiopacket);
+  Serial.print("Sending "); Serial.println(radiopacket);
+  Serial.println("Sending..."); delay(10);
+  radiopacket[6] = ' ';
+  radiopacket[7] = 'c';
+  radiopacket[8] = 'e';
+  radiopacket[9] = 'l';
+  radiopacket[10] = 's';
+  radiopacket[11] = 'i';
+  radiopacket[12] = 'u';
+  radiopacket[13] = 's';
+  //
+  rf95.send((uint8_t *)radiopacket, 20);
+
+  Serial.println("Waiting for packet to complete..."); delay(10);
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+  
+
+  Serial.println("Waiting for reply..."); delay(10);
+  if (rf95.waitAvailableTimeout(1000))
+  { 
+    //a reply message
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is there a listener around?");
+  }
+  delay(1000);
+}
+    
---
+++ Preston_sleep.ino
@@ -0,0 +1,34 @@
+
+#include <Adafruit_SleepyDog.h>
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) ; // wait for Arduino Serial Monitor (native USB boards)
+  Serial.println("Adafruit Watchdog Library Sleep Demo!");
+  Serial.println();
+}
+
+void loop() {
+  Serial.println("Going to sleep in one second...");
+  delay(1000);
+  
+  // To enter low power sleep mode call Watchdog.sleep() like below
+  // and the watchdog will allow low power sleep for as long as possible.
+  // The actual amount of time spent in sleep will be returned (in 
+  // milliseconds).
+  int sleepMS = 0;
+  for(int i = 0;i<4;i++)
+  {
+     sleepMS =+ Watchdog.sleep();
+    }
+ 
+
+  // Alternatively you can provide a millisecond value to specify
+  // how long you'd like the chip to sleep, but the hardware only
+  // supports a limited range of values so the actual sleep time might
+  // be smaller.  The time spent in sleep will be returned (in
+  // milliseconds).
+  // int sleepMS = Watchdog.sleep(1000);  // Sleep for up to 1 second.
+
+  Serial.print("I'm awake now!");
+}
